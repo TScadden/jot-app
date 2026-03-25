4dce5d4948217b8067ef59c54fa12646473e25ce
Tscadden <tysonscadden@gmail.com>
2026-03-25 14:29:34 -0600

    build: Initialize Android application build configuration and declare core dependencies using version catalogs.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,9 +9,7 @@ plugins {
 
 android {
     namespace = "com.notel.notel"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.notel.notel"
