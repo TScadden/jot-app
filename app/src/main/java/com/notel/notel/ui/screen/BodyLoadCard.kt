a04aea455bae6a8f99c47946a2d5bcd03e9d718e
Tscadden <tysonscadden@gmail.com>
2026-04-10 18:48:46 -0600

    Added missing LazyColumn imports in BodyLoadCard

--- app/src/main/java/com/notel/notel/ui/screen/BodyLoadCard.kt
+++ app/src/main/java/com/notel/notel/ui/screen/BodyLoadCard.kt
@@ -4,6 +4,8 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
