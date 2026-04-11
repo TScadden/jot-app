092930f3fa87d61110d9b6d7fe85ed348e37eb28
Tscadden <tysonscadden@gmail.com>
2026-04-10 20:24:06 -0600

    Fixed missing CircleShape import in BodyLoadScreen

--- app/src/main/java/com/notel/notel/ui/screen/BodyLoadScreen.kt
+++ app/src/main/java/com/notel/notel/ui/screen/BodyLoadScreen.kt
@@ -2,6 +2,7 @@ package com.notel.notel.ui.screen
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
