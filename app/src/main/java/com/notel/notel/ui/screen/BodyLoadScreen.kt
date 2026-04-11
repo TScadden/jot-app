90580667731f263e3e52b0208996932a12198e03
Tscadden <tysonscadden@gmail.com>
2026-04-10 23:40:53 -0600

    Added missing TextOverflow import for habit titles

--- app/src/main/java/com/notel/notel/ui/screen/BodyLoadScreen.kt
+++ app/src/main/java/com/notel/notel/ui/screen/BodyLoadScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
