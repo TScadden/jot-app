48659354898308bb5881bfeec47f1c27b2386e3b
Tscadden <tysonscadden@gmail.com>
2026-04-10 22:16:17 -0600

    Fixed missing RoundedCornerShape import in BodyLoadScreen

--- app/src/main/java/com/notel/notel/ui/screen/BodyLoadScreen.kt
+++ app/src/main/java/com/notel/notel/ui/screen/BodyLoadScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.*
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
