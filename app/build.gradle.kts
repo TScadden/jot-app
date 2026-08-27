5721311d0b4f3122c782100a5e6f93b1d58c86df
Tscadden <tysonscadden@gmail.com>
2026-08-27 14:40:42 -0600

    chore: bump versionCode to 14 and versionName to 2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.notel.notel"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.1"
+        versionCode = 14
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
